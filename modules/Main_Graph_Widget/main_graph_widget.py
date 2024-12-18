ce8331184dca39f4299b6a04fcf6feeacc230a73
Vladislav Komarov <67183850+AtariVC@users.noreply.github.com>
2024-12-19 01:19:02 +0700

    mod graph dialog

--- modules/Main_Graph_Widget/main_graph_widget.py
+++ modules/Main_Graph_Widget/main_graph_widget.py
@@ -15,9 +15,11 @@ from pathlib import Path
 # /src
 src_path = Path(__file__).resolve().parent.parent.parent
 modules_path = Path(__file__).resolve().parent.parent
+widgets_path = Path(__file__).resolve().parent.joinpath("Engine/widgets")
 # Добавляем папку src в sys.path
 sys.path.append(str(src_path))
 sys.path.append(str(modules_path))
+sys.path.append(str(widgets_path))
 
 from src.modbus_worker import ModbusWorker                          # noqa: E402
 from src.ddii_command import ModbusCMCommand, ModbusMPPCommand      # noqa: E402
@@ -25,6 +27,7 @@ from src.parsers import  Parsers                                    # noqa: E402
 from modules.Main_Serial.main_serial_dialog import SerialConnect    # noqa: E402
 from src.log_config import log_init                                 # noqa: E402
 from src.parsers_pack import LineEObj                               # noqa: E402
+from Engine.widgets.graph_widget import GraphWidget                               # noqa: E402
 
 
 class MainGraphWidget(QtWidgets.QDialog):
@@ -32,6 +35,7 @@ class MainGraphWidget(QtWidgets.QDialog):
     lineEdit_T_sipm                     : QtWidgets.QLineEdit
     pushButton_OK                       : QtWidgets.QPushButton
     vLayout_ser_connect                 : QtWidgets.QVBoxLayout
+    verticalLayout_graph               : QtWidgets.QVBoxLayout
     coroutine_get_temp_finished         = QtCore.pyqtSignal()
 
     CM_DBG_SET_CFG = 0x0005
@@ -56,12 +60,25 @@ class MainGraphWidget(QtWidgets.QDialog):
             self.cm_cmd: ModbusCMCommand = ModbusCMCommand(self.client, self.logger)
             self.mpp_cmd: ModbusMPPCommand = ModbusMPPCommand(self.client, self.logger)
         self.task = None # type: ignore
-        self.pushButton_OK.clicked.connect(self.pushButton_OK_handler)
-        self.coroutine_get_temp_finished.connect(self.creator_task)
-        # инициализация структур обновляемых полей приложения
-        self.le_obj: list[LineEObj] = self.init_linEdit_list()
+        # self.pushButton_OK.clicked.connect(self.pushButton_OK_handler)
+        # self.coroutine_get_temp_finished.connect(self.creator_task)
+        # # инициализация структур обновляемых полей приложения
+        # self.le_obj: list[LineEObj] = self.init_linEdit_list()
 
 
+    @asyncSlot()
+    async def get_client(self) -> None:
+        """Перехватывает client от SerialConnect и переподключается к нему"""
+        if self.w_ser_dialog.pushButton_connect_flag == 1:
+            self.client: AsyncModbusSerialClient = self.w_ser_dialog.client
+            await self.client.connect()
+            self.cm_cmd = ModbusCMCommand(self.client, self.logger)
+        if self.w_ser_dialog.pushButton_connect_flag == 0:
+            if self.task:
+                self.task.cancel()
+
+        if self.w_ser_dialog.status_CM == 1:
+            self.coroutine_get_temp_finished.emit()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
@@ -69,7 +86,8 @@ if __name__ == "__main__":
     # light(app)
     logger = log_init()
     w_ser_dialog: SerialConnect = SerialConnect(logger)
-    w: MainTermDialog = MainTermDialog(logger, w_ser_dialog)
+    graph_widget: GraphWidget = GraphWidget()
+    w: MainGraphWidget = MainGraphWidget(logger, w_ser_dialog)
 
     spacer_g = QSpacerItem(0, 0, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
     spacer_v = QSpacerItem(0, 0, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
@@ -89,6 +107,7 @@ if __name__ == "__main__":
     gridL.addItem(spacer_g, 0, 2)
     gridL.addItem(spacer_v, 2, 1, 1, 3)
     gridL.addWidget(w_ser_dialog, 0, 1)
+    w.verticalLayout_graph.addWidget(graph_widget)
 
     event_loop = qasync.QEventLoop(app)
     asyncio.set_event_loop(event_loop)
